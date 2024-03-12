72878f99723f457e2507d30be2e5cc71191ada46
chanelcolgate <57675195+chanelcolgate@users.noreply.github.com>
2024-03-12 09:23:48 +0700

    Update exceptions.py

--- starlette/exceptions.py
+++ starlette/exceptions.py
@@ -13,19 +13,21 @@ class HTTPException(Exception):
         status_code: int,
         detail: str | None = None,
         headers: dict[str, str] | None = None,
+        result: str | None = None,
     ) -> None:
         if detail is None:
             detail = http.HTTPStatus(status_code).phrase
         self.status_code = status_code
         self.detail = detail
         self.headers = headers
+        self.result = result
 
     def __str__(self) -> str:
-        return f"{self.status_code}: {self.detail}"
+        return f"{self.status_code}: {self.detail}, {self.result}"
 
     def __repr__(self) -> str:
         class_name = self.__class__.__name__
-        return f"{class_name}(status_code={self.status_code!r}, detail={self.detail!r})"
+        return f"{class_name}(status_code={self.status_code!r}, detail={self.detail!r}, result={self.result!r)"
 
 
 class WebSocketException(Exception):
